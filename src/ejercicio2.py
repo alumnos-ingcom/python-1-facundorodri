c2bcb5e63cb94abf03eaf0f97f8b6da434824e36
Facu <facu18azul@gmail.com>
2022-06-28 11:34:55 -0300

    le agregue un docstring a una funcion y le cambie el lugar al docstring de la funcion principal()

--- src/ejercicio2.py
+++ src/ejercicio2.py
@@ -7,6 +7,10 @@
 2. Escribir una función que reciba un número e indique si el mismo es positivo, negativo o cero utilizando sumas y restas.
 """
 def signo(numero):
+    """"
+    Esta funcion suma un cero al numero ingresado para ver si es igual a cero, por descarte verifica que si sumando uno al numero ingresado
+    sea positivo, y si no es cero o postivo, es negativo.
+    """
     if numero+0 == 0:
         sgn = 0
     elif numero+1 > 1:
@@ -16,6 +20,11 @@ def signo(numero):
     return(sgn)
         
 def principal():
+    """
+    Esta función es la que se encarga de la parte 'interactiva' del ejercicio
+    (La entrada, la llamada al algoritmo y la salida)
+    """
+
     numero = int(input("ingrese un numero"))
     signo_numero = signo(numero)
     if signo_numero == 0:
@@ -25,10 +34,6 @@ def principal():
     else:
         print("es negativo")
 
-"""
-Esta función es la que se encarga de la parte 'interactiva' del ejercicio
-(La entrada, la llamada al algoritmo y la salida)
-"""
 
 if __name__ == "__main__":
     principal()
